520e138a7a871abfcd9495fef31c8e0c076dca39
Kevin <102241392+Crossbow007@users.noreply.github.com>
2022-09-18 22:31:41 -0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+Name: Kevin Gao
+Purpose:Our first program in COMM1100
+*/
+int main()
+{
+	double area;
+	area = 3. / 2.;
+	cout << area << endl;
+	system("pause");
+}
